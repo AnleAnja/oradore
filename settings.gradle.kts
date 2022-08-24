c0c6179d237a7129a904bca37fdbe747cce77af1
Alexander Dobrynin <info@alex-dobrynin.com>
2022-08-24 13:51:46 +0200

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "oradore"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/oradore/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.oradore
+
+expect class Platform() {
+    val platform: String
+}
