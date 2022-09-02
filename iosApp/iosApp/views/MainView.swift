602b1142b6a4518170a7c06857f242b13db2b06a
Alexander Dobrynin <info@alex-dobrynin.com>
2022-09-02 09:41:31 +0200

    adds icons

--- iosApp/iosApp/views/MainView.swift
+++ iosApp/iosApp/views/MainView.swift
@@ -6,20 +6,20 @@ struct MainView: View {
     TabView {
       ProgramEntriesView()
         .tabItem {
-          Label("Programm", systemImage: "1.square.fill")
+          Label("Programm", systemImage: "list.bullet")
         }
         .tag(1)
       SpeakersView()
         .tabItem {
-          Label("Speaker", systemImage: "1.square.fill")
+          Label("Speaker", systemImage: "person.2")
         }.tag(2)
       RoomsView()
         .tabItem {
-          Label("Räume", systemImage: "1.square.fill")
+          Label("Räume", systemImage: "mappin.circle")
         }.tag(3)
       FavoritesView()
         .tabItem {
-          Label("Favoriten", systemImage: "1.square.fill")
+          Label("Favoriten", systemImage: "star")
         }.tag(4)
     }
   }
