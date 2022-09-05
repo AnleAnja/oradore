e10220fa64062f7dec9f3bb91c1a62072ce08549
Alexander Dobrynin <info@alex-dobrynin.com>
2022-09-05 22:21:10 +0200

    adds shared http client with GET /program, GET /rooms and GET /speakers

--- shared/src/commonMain/kotlin/com.example.oradore.api/NetworkApi.kt
+++ shared/src/commonMain/kotlin/com.example.oradore.api/NetworkApi.kt
@@ -1,6 +1,8 @@
 package com.example.oradore.api
 
 import com.example.oradore.models.ProgramEntryPreview
+import com.example.oradore.models.Room
+import com.example.oradore.models.Speaker
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.plugins.contentnegotiation.*
@@ -17,6 +19,15 @@ class NetworkApi(private val baseUrl: String) {
         }
     }
 
+    @Throws(Exception::class)
     suspend fun fetchProgramEntries(): List<ProgramEntryPreview> =
         http.get("$baseUrl/program").body()
+
+    @Throws(Exception::class)
+    suspend fun fetchSpeakers(): List<Speaker> =
+        http.get("$baseUrl/speakers").body()
+
+    @Throws(Exception::class)
+    suspend fun fetchRooms(): List<Room> =
+        http.get("$baseUrl/rooms").body()
 }
